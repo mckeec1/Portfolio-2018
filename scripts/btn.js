35225bb3627a384d800056f2f102819ab3bfd85c
mckeec1 <connermckee01@gmail.com>
2018-05-26 13:02:57 -0400

    Styles and functionality of go to top button

---
+++ scripts/btn.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {        
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        $('#return-to-top').fadeOut(200);   
+    }
+});
+$('#return-to-top').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                       
+    }, 500);
+});
